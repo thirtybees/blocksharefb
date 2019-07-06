9c2b9c07ab496d14ca7adef209d29d0097e32e8d
Petr Hučík <petr@getdatakick.com>
2019-07-06 10:37:01 +0200

    Release version 2.0.2

--- blocksharefb.php
+++ blocksharefb.php
@@ -32,7 +32,7 @@ class blocksharefb extends Module
 	{
 		$this->name = 'blocksharefb';
 		$this->tab = 'front_office_features';
-		$this->version = '2.0.1';
+		$this->version = '2.0.2';
 		$this->author = 'thirty bees';
 		$this->need_instance = 0;
 
